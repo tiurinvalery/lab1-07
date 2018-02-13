7b635d3b57f92e3fc4db33f38a8f36711bca05e0
Nechkina <ni2403kalos@gmail.com>
2018-02-13 10:23:23 +0200

    newBubble

--- src/SorterContainer.java
+++ src/SorterContainer.java
@@ -71,4 +71,24 @@ public class SorterContainer {
         }
     }
 
+    public static void newBubbleSort(int[] arr) {
+        boolean swapped = true;
+        int j = 0;
+        int tmp;
+        while (swapped) {
+            swapped = false;
+            j++;
+            for (int i = 0; i < arr.length - j; i++) {
+
+                if (arr[i] > arr[i + 1]) {
+                    tmp = arr[i];
+                    arr[i] = arr[i + 1];
+                    arr[i + 1] = tmp;
+                    swapped = true;
+
+                }
+            }
+        }
+    }
+
 }
